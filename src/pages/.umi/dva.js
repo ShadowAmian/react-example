9d8be0974a129381ec389bfcbc7f94627c637dc7
七街城酒 <boringmian@outlook.com>
2020-01-03 16:56:53 +0800

    feat：init

---
+++ src/pages/.umi/dva.js
@@ -0,0 +1,38 @@
+import dva from 'dva';
+import { Component } from 'react';
+import createLoading from 'dva-loading';
+import history from '@tmp/history';
+
+let app = null;
+
+export function _onCreate() {
+  const plugins = require('umi/_runtimePlugin');
+  const runtimeDva = plugins.mergeConfig('dva');
+  app = dva({
+    history,
+    
+    ...(runtimeDva.config || {}),
+    ...(window.g_useSSR ? { initialState: window.g_initialData } : {}),
+  });
+  
+  app.use(createLoading());
+  (runtimeDva.plugins || []).forEach(plugin => {
+    app.use(plugin);
+  });
+  
+  app.model({ namespace: 'counter', ...(require('G:/代码/个人代码/github仓库/umi/umi-examples/typescript/src/models/counter.ts').default) });
+app.model({ namespace: 'global', ...(require('G:/代码/个人代码/github仓库/umi/umi-examples/typescript/src/models/global.ts').default) });
+  return app;
+}
+
+export function getApp() {
+  return app;
+}
+
+export class _DvaContainer extends Component {
+  render() {
+    const app = getApp();
+    app.router(() => this.props.children);
+    return app.start()();
+  }
+}
